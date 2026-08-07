acfe855025b15e24b4cf14be9d8f2f218b6dbf23
nelavaihema <nelavaihema@gmail.com>
2026-08-07 15:56:47 +0530

    resume

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,6 @@ const links = [
   { label: "Skills", href: "#skills" },
   { label: "Projects", href: "#projects" },
   { label: "Experience", href: "#experience" },
-  { label: "Resume", href: "/resume.html" },
   { label: "Contact", href: "#contact" },
 ];
 
